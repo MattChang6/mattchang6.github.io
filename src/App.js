362750a72f088f5be334f5fa533c27b522b75bc5
mattc <mattchang6@gmail.com>
2024-08-24 17:21:32 -0700

    implement construction function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from './components/header.js';
 import Footer from './components/footer.js';
 import { Route, Routes } from "react-router-dom";
-import { Home } from './pages/home';
-import { Resources } from './pages/resources';
-import { Contact } from './pages/contact';
-import { Services } from './pages/services';
+import { Home } from './pages/home.js';
+import { Resources } from './pages/resources.js';
+import { Contact } from './pages/contact.js';
+import { Services } from './pages/services.js';
+import { Construction } from './pages/construction.js';
 import './App.css';
 
 
@@ -13,14 +14,15 @@ function App() {
 	return (
 		<>
 		<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-		<Header />
+		<Construction />
+{/*		<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/resources" element={<Resources />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="/services" element={<Services />} />
 			</Routes>
-		<Footer />
+		<Footer />*/}
 		</>
 	);
 }
