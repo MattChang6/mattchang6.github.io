012da07a8185fa4fbbbd8bf576089db06a7de23a
mattc <mattchang6@gmail.com>
2024-09-05 19:30:35 -0700

    update website

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -7,15 +7,15 @@ export function Resources() {
             <h1 className="resource-title">Resources</h1>
             <div>
                 <div className='resource-grid'>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                         <p className='resource-description'>Internal Revenue Service<br />
                         <a className='resource-link' href='https://www.irs.gov/' target='_blank' rel="noreferrer">https://www.irs.gov/</a></p>
                     </div>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                     <p className='resource-description'>Social Security<br />
                     <a className='resource-link' href='https://www.ssa.gov/' target='_blank' rel="noreferrer">https://www.ssa.gov/</a></p>
                     </div>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                     <p className='resource-description'>Quickbooks<br />
                     <a className='resource-link' href='https://proadvisor.intuit.com/app/accountant/search?searchId=jay-chang' target='_blank' rel="noreferrer">Link</a></p>
                     </div>
@@ -23,15 +23,15 @@ export function Resources() {
             </div>
             <div>
                 <div className='resource-grid'>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                         <p className='service-subtitle'>Verifyle</p>
                         <p className='service-description'></p>
                     </div>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                         <p className='service-subtitle'>Link Title</p>
                         <p className='service-description'></p>
                     </div>
-                    <div className='item1-b2'>
+                    <div className='item1-resource'>
                         <p className='service-subtitle'>Link Title</p>
                         <p className='service-description'></p>
                     </div>
